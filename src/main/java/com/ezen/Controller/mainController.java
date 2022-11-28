edf4f6ce6dee9a68dcad1a127de2f4ce5d9c0f9e
KimKyungmin84 <112397472+KimKyungmin84@users.noreply.github.com>
2022-11-28 21:29:25 +0900

    co

--- src/main/java/com/ezen/Controller/mainController.java
+++ src/main/java/com/ezen/Controller/mainController.java
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-package com.ezen.Controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class mainController {
-    @RequestMapping("main/main.do")
-    public String main_main(){
-        System.out.println("1");
-        return "main";
-    }
-    @RequestMapping("goods/list.do")
-    public String board_list(){
-        return "goodsList";
-    }
-    @RequestMapping("notice/list.do")
-    public String notice_list(){
-        return "notice";
-    }
-=======
 package com.ezen.Controller;
 
 import org.springframework.stereotype.Controller;
@@ -40,5 +18,4 @@ public class mainController {
     public String notice_list(){
         return "notice";
     }
->>>>>>> branch 'master' of https://github.com/kimmongja/omc.git
 }
